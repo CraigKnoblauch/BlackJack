b31b81ac12ff6b7ee82a0cb1dc6b5287fae5f963
Craig Knoblauch <craigknoblauch@gmail.com>
2018-10-03 17:42:11 -0700

    fixed ace problem

--- blackjack.py
+++ blackjack.py
@@ -3,10 +3,6 @@ from gym import spaces
 from gym.utils import seeding 
 import random
 
-# Closest I can get to #define
-STAY = 0
-HIT = 1
-
 ## 
 # Defining the deck in this way will allow us to render easily
 # and will provide and "infinite" deck
@@ -23,9 +19,44 @@ deck = {'1': 1,
         'J': 10,
         'Q': 10,
         'K': 10,
-        'A': [1, 11]
+        'A': 0,
+        'A1': 1,
+        'A11': 11
        }
 
+##
+# Since the ace can take on multiple values, this class allows us to organize the duality
+class Ace():
+
+    def __init__(self, val):
+        self.value = val
+
+    def getValue(self):
+        return self.value
+
+    def setValue(self, val):
+        self.value = val
+
+    # Chooses an appropriate value for an ace, based on the current hand
+    def chooseValue(self, hand):
+        val = 1
+
+        # Sum the hand thus far
+        hand_sum = 0
+        for deck_key in hand:
+            hand_sum += deck[deck_key]
+
+        # Determine the value of ace that will prevent the holder from busting. If no such value exists, value remains at 1
+        if (hand_sum + 11) <= 21:
+            val = 11
+
+        return val
+
+        
+
+# Closest I can get to #define
+STAY = 0
+HIT = 1
 
 
 ## "Private" functions
@@ -41,24 +72,24 @@ class BlackJack(gym.Env):
 
     def __init__(self):
         # Init the player and the dealer's hand
-        self.player_hand = random.sample( list(deck), 2 ) 
-        self.dealer_hand = random.sample( list(deck), 2 ) 
+        player_hand = random.sample( list(deck), 2 ) 
+        dealer_hand = random.sample( list(deck), 2 ) 
 
-        self.player_sum = self.getSum(self.player_hand)
-        self.dealer_sum = self.getSum(self.dealer_hand)
+        player_sum = self.getSum(player_hand)
+        dealer_sum = self.getSum(dealer_hand)
 
         # I need a way to track if the dealer has flipped over his card
         # To do this, the first position of the dealer's hand will be used 
         # to indicate. If index 0 of the dealer's hand is 0, he as not flipped
         # his card, if it is < 0, he has. This assures the sum function cannot confuse
         # the object in the first postion with a card.
-        (self.dealer_hand).insert(0, 0)
+        (dealer_hand).insert(0, 0)
 
         self.action_space = spaces.Discrete(2) # 0 is stay, 1 is hit
-        self.observation_space = [self.player_hand,
-                                  self.player_sum,
-                                  self.dealer_hand,
-                                  self.dealer_sum
+        self.observation_space = [player_hand,
+                                  player_sum,
+                                  dealer_hand,
+                                  dealer_sum
                                   ]
 
         self.observation = self.observation_space # I think my observation understanding is breaking down
@@ -75,7 +106,7 @@ class BlackJack(gym.Env):
         # Recall this is a discrete action space where 0 means "stay" and 1 means "hit"
         if action == HIT:
             self.giveCardToPlayer()
-            if self.isBust( self.player_hand ):
+            if self.isBust( self.getPlayerHand() ):
                 self.updatePlayerSum()
                 done = True
                 reward = -10
@@ -86,14 +117,14 @@ class BlackJack(gym.Env):
             # Flip the dealer's card and update his sum
             self.revealDealerCard()
             self.updateDealerSum()
-            while not self.isBust( self.dealer_hand ) and self.getDealerSum() <= 16:
+            while not self.isBust( self.getDealerHand() ) and self.getDealerSum() <= 16:
                 self.giveCardToDealer()
                 self.updateDealerSum()
 
             # Dealer is staying
             done = True
 
-            if self.isBust( self.dealer_hand ):
+            if self.isBust( self.getDealerHand() ):
                 # Player wins
                 reward = 10
             
@@ -122,16 +153,26 @@ class BlackJack(gym.Env):
         return bust
     
     def giveCardToPlayer(self):
-        ( (self.observation)[0] ).append( random.sample( list(deck), 1 ) )
+        card = random.sample( list(deck), 1 )
+        # Determine if card is an ace. If it is, determine and set its value, based on the current sum of the hand
+        if card == 'A':
+            ace_obj = Ace(0)
+            val_of_ace = ace_obj.chooseValue( self.getPlayerHand() )
+            if val_of_ace == 1:
+                card = 'A1'
+            else:
+                card = 'A11'
+
+        ( (self.observation)[0] ).append( card )
 
     def giveCardToDealer(self):
-        ( (self.observation)[2] ).append( random.sample( list(deck), 1 ) )
+        ( (self.observation)[2] ).append( random.sample( list(deck[:-2]), 1 ) )
 
     def updatePlayerSum(self):
-        ( (self.observation)[1] ) = self.getSum(self.player_hand)
+        ( (self.observation)[1] ) = self.getSum(self.getPlayerHand())
 
     def updateDealerSum(self):
-        ( (self.observation)[3] ) = self.getSum(self.dealer_hand)
+        ( (self.observation)[3] ) = self.getSum(self.getDealerHand())
 
     def revealDealerCard(self):
         ( (self.observation)[2] )[0] = -1
